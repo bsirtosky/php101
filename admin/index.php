ad7ac2bc2d0844b22aa8bf66ccea20695dcf6d9e
Bryan Sirtosky <bryan.sirtosky@gmail.com>
2013-11-23 23:30:02 -0500

    Fix files.

--- admin/index.php
+++ admin/index.php
@@ -2,22 +2,14 @@
 
 require '../includes/form.php';
 
-<<<<<<< HEAD
 $form_fields = array('username', 'password');    
 $required_fields = array('username', 'password');    
-=======
-$form_fields = array('username', 'password');
-$required_fields = array('username', 'password');
->>>>>>> ce8c39d1871dde3e4f18ccbd216eb5d091d95dbf
 
 $form = form_get_data($form_fields, $_POST);
 $valid = form_is_valid($form, $required_fields);
 
 ?>
-<<<<<<< HEAD
 
-=======
->>>>>>> ce8c39d1871dde3e4f18ccbd216eb5d091d95dbf
 <!DOCTYPE html>
 <html>
     <head>
@@ -27,17 +19,10 @@ $valid = form_is_valid($form, $required_fields);
         <pre><?php var_dump($form); ?></pre>
         <?php if ($valid) {
             ?>
-<<<<<<< HEAD
             <div> this form is valid.</div>
-            <?php }
-            ?>
-=======
-            <div>
-                This form is valid!!
-            </div>
-            <?php
-        } ?>
->>>>>>> ce8c39d1871dde3e4f18ccbd216eb5d091d95dbf
+		<?php }
+        ?>
+
         <form method="post">
             <ul>
                 <li>
@@ -46,21 +31,12 @@ $valid = form_is_valid($form, $required_fields);
                 </li>
                 <li>
                     <label for="password">Password</label>
-<<<<<<< HEAD
                     <input type="text" name="password" />
                 </li>
                 <li>
                     <input type="submit" name="Submit" />
                 </li>
             <ul>    
-=======
-                    <input type="password" name="password" />
-                </li>
-                <li>
-                    <input type="submit" value="Submit" />
-                </li>
-            </ul>
->>>>>>> ce8c39d1871dde3e4f18ccbd216eb5d091d95dbf
         </form>
     </body>
 </html>
--- includes/form.php
+++ includes/form.php
@@ -2,11 +2,7 @@
 
 function form_get_data($form_fields, $request) {
     $return_value = array();
-<<<<<<< HEAD
-    
-=======
 
->>>>>>> ce8c39d1871dde3e4f18ccbd216eb5d091d95dbf
     foreach ($form_fields as $field_name) {
         $return_value[$field_name] = isset($request[$field_name]) ? $request[$field_name] : '';
     }
@@ -16,24 +12,15 @@ function form_get_data($form_fields, $request) {
 
 function form_is_valid($form, $required_fields) {
     $valid = true;
-<<<<<<< HEAD
-=======
 
->>>>>>> ce8c39d1871dde3e4f18ccbd216eb5d091d95dbf
     foreach ($required_fields as $field_name) {
         if ($form[$field_name] === '') {
             $valid = false;
             break;
-<<<<<<< HEAD
         }   
     }
+
+	return $valid;
 }
 
 ?>
-=======
-        }
-    }
-
-    return $valid;
-}
->>>>>>> ce8c39d1871dde3e4f18ccbd216eb5d091d95dbf
